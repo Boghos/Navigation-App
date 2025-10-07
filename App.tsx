42cb4b297e49a319699ad6d1a249b096d1427cd9
Boghos <bhamalian@students.haigazian.edu.lb>
2025-10-07 17:16:07 +0300

    After testing the screen work with App.tsx, I imported them to index.tsx for cleaner App.tsx

--- App.tsx
+++ App.tsx
@@ -1,34 +1,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import SplashScreen from './src/screens/SplashScreen';
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import EnterCompanyIDScreen from './src/screens/EnterCompanyIDScreen';
-import PickVoiceScreen from './src/screens/PickVoiceScreen';
-import MainScreen from './src/screens/MainScreen';
-import { useState, useEffect } from 'react';
-
-type RootStackParamList = {
-  Splash: undefined;
-  Welcome: undefined;
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App = () => {
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Splash" component={SplashScreen} />
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
+  return;
 };
 
 export default App;
