db42877f162275425641d3fb388a02ff1ed01b4c
Boghos <bhamalian@students.haigazian.edu.lb>
2025-10-08 14:11:22 +0300

    imported and added icons library to add settings icon in the Main screen

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,14 +1,15 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, TouchableOpacity } from 'react-native';
 import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const MainScreen = ({ navigation }: any) => {
   return (
     <View>
       <Text>MainScreen</Text>
-      <Button
-        title="Open Settings"
-        onPress={() => navigation.navigate('SettingsModal')}
-      />
+      <TouchableOpacity onPress={() => navigation.navigate('SettingsModal')}>
+        <Ionicons name="settings-outline" size={24} color="#000" />
+        <Text>Open Settings</Text>
+      </TouchableOpacity>
       <Button
         title="Launch Voice bot"
         onPress={() => navigation.navigate('VoiceChatModal')}
