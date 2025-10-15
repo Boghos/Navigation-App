d37bccfb0cd046799d6d79cee45ab0081aea90c6
Boghos <bhamalian@students.haigazian.edu.lb>
2025-10-15 15:10:53 +0300

    UI improvement: imrpoved splash screen UI

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
 const Splashscreen = () => {
   return (
-    <View>
-      <Text>Splash Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Navigation App</Text>
     </View>
   );
 };
 
 export default Splashscreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', // center vertically
+    alignItems: 'center', // center horizontally
+    backgroundColor: '#f8f9fa', // light background
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: '700',
+    color: '#007AFF',
+    marginBottom: 30,
+  },
+});
